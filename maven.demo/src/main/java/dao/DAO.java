855de4a92b407037f57dd26c5bcb1d9dc0fd7d51
Filipi Pereira <filipiacademico@gmail.com>
2024-08-23 11:48:32 -0300

    arrumei detalhes exercicios2

--- maven.demo/src/main/java/dao/DAO.java
+++ maven.demo/src/main/java/dao/DAO.java
@@ -18,7 +18,7 @@ public class DAO {
 		int porta = 5432;
 		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
 		String username = "postgres";
-		String password = "@Alunocefet15";
+		String password = "x";
 		boolean status = false;
 
 		try {
