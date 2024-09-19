e35ce67a998069760f0974aac10314ec916499c2
Filipi Pereira <filipiacademico@gmail.com>
2024-09-19 16:50:56 -0300

    finalizei exercicio 3

--- WS03ProdutoService/src/main/java/dao/DAO.java
+++ WS03ProdutoService/src/main/java/dao/DAO.java
@@ -16,7 +16,7 @@ public class DAO {
 		int porta = 5432;
 		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
 		String username = "postgres";
-		String password = "@Alunocefet15";
+		String password = "";
 		boolean status = false;
 
 		try {
